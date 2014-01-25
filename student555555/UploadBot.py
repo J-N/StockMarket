9bd77e0b7f7e5caef46116d69834a17428c5c611
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-01-25 15:50:08 -0500

    Add new student

---
+++ student555555/UploadBot.py
@@ -0,0 +1,24 @@
+#generates a student bot
+
+import os
+import sys
+import inspect
+
+id = raw_input('Please enter your student ID number: ')
+
+print 'Student ID: ',id
+
+student = open('studentCode'+id+'.txt')
+
+temp  = open('../studentTemplate.gen', 'r')
+output = open('../bots/student'+str(id)+'.py', 'w')
+
+newcode  = temp.read().replace('###',id) + '\n' + student.read()
+
+output.write(newcode)
+output.close()
+
+temp.close()
+
+student.close()
+sys.exit()
