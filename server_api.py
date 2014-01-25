6ccc12b9b631fc8195279a2ea8091cd2a66453ba
mtae <aahlad.asg@gmail.com>
2014-01-24 20:06:02 -0500

    v1 of client/server interaction

---
+++ server_api.py
@@ -0,0 +1,25 @@
+"""
+Interface:
+
+10 stocks:
+    0..9
+
+Stock: symbol, volume, bid-ask price
+        
+Public functions: (available to client)
+
+getAll() : list of symbols
+
+getInfo(string symbol) : returns the bid ask price
+
+buy(string symbol, int quantity): returns 1/0 (for accept/decline)
+
+sell(string symbol, int quantity): returns 1/0 (for accept/decline)
+
+getAccount()
+
+getPortfolio()
+ 
+ 
+
+"""
