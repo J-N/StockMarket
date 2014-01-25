de77169fabcfe6ea61eab27ead0465b33359c35c
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-01-25 14:18:53 -0500

    Updated botgenerator

--- botgenerator.py
+++ botgenerator.py
@@ -1,12 +1,18 @@
-#generates a bot
+#generates a number of bots
 
 import os
+import sys
 import random
 
-botNumber = 12345
+number = 10
 
-temp  = open('randomTemplate.gen', 'r')
-output = open('./bots/bot'+str(botNumber)+'.py', 'w')
-clean  = temp.read().replace("###", str(random.randint(0,100)*.01))
-output.write(clean)
-output.close()
+x =  000001
+while (x <= number):
+	botNumber = x
+	temp  = open('randomTemplate.gen', 'r')
+	output = open('./bots/bot'+str(botNumber).zfill(6)+'.py', 'w')
+	clean  = temp.read().replace("###", str(random.randint(0,100)*.01))
+	output.write(clean)
+	output.close()
+	x += 1
+sys.exit()
