99199a874745fecf9d5277d060842996dbf50d58
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-01-25 12:57:01 -0500

    Move template

--- botgenerator.py
+++ botgenerator.py
@@ -6,6 +6,7 @@ import random
 botNumber = 12345
 
 temp  = open('randomTemplate.py', 'r')
-output = open('./bots/bot'+str(botNumber), 'w')
+output = open('./bots/bot'+str(botNumber)+'.py', 'w')
 clean  = temp.read().replace("###", str(random.randint(0,100)*.01))
 output.write(clean)
+output.close()
